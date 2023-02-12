df1178f1aaba0a0d7842d4cdf85d7be84a9ac75f
Esbree <noelva@gmx.de>
2023-02-12 11:21:47 +0100

    created computerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-console.log('Hello World!');
+function getComputerChoice (){
+    const words = ['Rock', 'Paper', 'Scissors'];
+    let r = Math.floor(Math.random() * 3); // random number between from 0 to 2
+    return words[r];
+}
