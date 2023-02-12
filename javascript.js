3dcc56c6b25242b9d2dc6f95422f2f467f665431
Esbree <noelva@gmx.de>
2023-02-12 11:58:39 +0100

    created playRound function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,33 @@
 function getComputerChoice (){
-    const words = ['Rock', 'Paper', 'Scissors'];
+    const words = ['rock', 'paper', 'scissors'];
     let r = Math.floor(Math.random() * 3); // random number between from 0 to 2
     return words[r];
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    let result = '';
+
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        result = 'Tie!';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        result = 'You lose. Paper beats Rock!';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        result = 'You win. Rock beats Scissors!';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        result = 'You win. Paper beats Rock!';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        result = 'Tie!';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        result = 'You lose. Scissors beats Paper.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        result = 'You lose. Rock beats Scissors';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        result = 'You win. Scissors beats Paper.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        result = 'Tie!';
+    }
+
+    return result;
+}
+   
